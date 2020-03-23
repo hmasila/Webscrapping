8eefa4b72fabba5161c4fb65ab735bf924653e23
Hannah Masila <hannahmasila@gmail.com>
2020-03-23 08:35:02 +0300

    more optimized(memory-wise) web scrapper)

--- luxury_sedan_scrapper.py
+++ luxury_sedan_scrapper.py
@@ -6,19 +6,14 @@ br = mechanize.Browser()
 br.set_handle_robots(False)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-links = []
-edmunds_forum = 'https://forums.edmunds.com/discussion/2864/general/x/entry-level-luxury-performance-sedans'
-links.append(edmunds_forum)
-
-for i in range(2,301):
-    links.append(edmunds_forum+'/p'+str(i))
+url = 'https://forums.edmunds.com/discussion/2864/general/x/entry-level-luxury-performance-sedans/p433'
+page = br.open(url)
 
 with open('comments.csv', 'a') as csv_file:
     writer = csv.writer(csv_file)
-
-    for pg in links:
-        print(pg)
-        page = br.open(pg)
+    while True:
+        print(url)
+        page = br.open(url)
         soup = BeautifulSoup(page.read(),'html.parser')
         table = soup.find('ul', attrs={'class': 'MessageList DataList Comments'})
 
@@ -33,5 +28,10 @@ with open('comments.csv', 'a') as csv_file:
             message_box = row.find_next('div', attrs={'class': 'Message'})
             message = message_box.text.strip()
 
-            writer.writerow([date, userid, message])    
+            writer.writerow([date, userid, message])
 
+        try:
+            url = br.follow_link(text_regex=r"»").geturl()
+        except:
+            print("this is the last page")
+            break
